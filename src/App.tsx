c3914ba2e566b734a40336115deee9c3dc84b4d6
ambrsumn <beingambersuman@gmail.com>
2025-05-24 23:32:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+import BuildingHome from './Components/BuildingHome';
+
+function App() {
+  return (
+    <div className="App bg-black h-[100vh] w-full">
+      
+      {/* NAVBAR */}
+
+
+      {/* MAIN  */}
+      <div className=' w-[80%] mx-auto h-full'>
+      <BuildingHome />
+      </div>
+
+
+      {/* FOOTER  */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BuildingHome.tsx
@@ -0,0 +1,22 @@
+// src/Components/BuildingHome.tsx
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function BuildingModel() {
+  const { scene } = useGLTF('/models/building.glb');
+  return <primitive object={scene} scale={1} />;
+}
+
+export default function BuildingHome() {
+  return (
+    <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[5, 5, 5]}/>
+      <Suspense fallback={null}>
+        <BuildingModel />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+}
